fca3ca61232d9370c18f7aff15f73ff4c7b1c600
bigfinsmall <fsmall90@gmail.com>
2023-11-07 16:44:33 -0500

    Obsidian and Marksman

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -30,17 +30,17 @@ local opts = {
   -- * keyword: highlights of the keyword
   -- * after: highlights after the keyword (todo text)
   highlight = {
-    multiline = true,               -- enable multine todo comments
-    multiline_pattern = "^.",       -- lua pattern to match the next multiline from the start of the matched keyword
-    multiline_context = 10,         -- extra lines that will be re-evaluated when changing a line
-    before = "",                    -- "fg" or "bg" or empty
-    keyword = "wide",               -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-    after = "fg",                   -- "fg" or "bg" or empty
-    -- pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
-    pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
-    comments_only = true,           -- uses treesitter to match keywords in comments only
-    max_line_len = 400,             -- ignore lines longer than this
-    exclude = {},                   -- list of file types to exclude highlighting
+    multiline = true,                -- enable multine todo comments
+    multiline_pattern = "^.",        -- lua pattern to match the next multiline from the start of the matched keyword
+    multiline_context = 10,          -- extra lines that will be re-evaluated when changing a line
+    before = "",                     -- "fg" or "bg" or empty
+    keyword = "wide",                -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+    after = "fg",                    -- "fg" or "bg" or empty
+    pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+    --pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+    comments_only = true,            -- uses treesitter to match keywords in comments only
+    max_line_len = 400,              -- ignore lines longer than this
+    exclude = {},                    -- list of file types to exclude highlighting
   },
   -- list of named colors where we try to extract the guifg from the
   -- list of highlight groups or use the hex color if hl not found as a fallback
@@ -63,8 +63,8 @@ local opts = {
     },
     -- regex that will be used to match keywords.
     -- don't replace the (KEYWORDS) placeholder
-    --   pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-    pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
   },
 }
 return opts
