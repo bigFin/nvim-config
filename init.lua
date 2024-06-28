3ff7002065fe4d574be85e67331e950498465d03
bigfinsmall <fsmall90@gmail.com>
2024-06-28 18:27:56 -0400

    up

--- init.lua
+++ init.lua
@@ -55,6 +55,39 @@ require('lazy').setup({
     end,
   },
   {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "/Storage/logseq",
+        },
+        {
+          name = "work",
+          path = "/Code/avenueIntelligence/vault",
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
+  },
+  {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     requires = { { "nvim-lua/plenary.nvim" } }
@@ -294,7 +327,10 @@ require('lazy').setup({
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   require 'plugins.trouble',
   require 'plugins.twilight',
-  -- require 'plugins.obsidian',
+  config = function()
+    require("plugins.obsidian").setup {}
+  end,
+  require 'plugins.obsidian',
   -- require 'plugins.neorg',
   'ekickx/clipboard-image.nvim',
   -- require 'plugins',
